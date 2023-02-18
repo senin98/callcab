763b55f543e631a36ed7c87448c63cd27f8c33a2
senin98 <seninhm@gmail.com>
2023-02-18 22:34:11 +0300

    test

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:flutter/material.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  String message;
 
   runApp(const MyApp());
 }
